416527b354b9dab986a39c466ce40777145524db
shixins <shixins@mit.edu>
2025-05-22 18:56:11 -0400

    [chore] kunpeng's changes to script

--- scripts/verilator/run.py
+++ scripts/verilator/run.py
@@ -1,57 +1,49 @@
-
 import sys, os
 
 
 if __name__ == "__main__":
+    # STEP
+    if len(sys.argv) <= 3:
+        print("3 arguments needed!")
+        print("Usage: " + sys.argv[0] + " top_verilog.v includeDir topModule [define]")
+        exit()
+    if len(sys.argv) == 4:
+        _, top_verilog, includeDir, topModule = sys.argv
+        define = ""
+    else:
+        _, top_verilog, includeDir, topModule, define = sys.argv
 
-  # STEP
-  if len(sys.argv) <= 3:
-    print("3 arguments needed!")
-    print("Usage: " + sys.argv[0] + " top_verilog.v includeDir topModule define")
-    exit()
-  
-  top_verilog = sys.argv[1]
-  build_folder_name = top_verilog[4:-2].replace("/", "-")
+    build_folder_name = ".".join("-".join(top_verilog.split("/")[1:]).split(".")[0:-1])
 
-  includeDir  = "-I%s" % sys.argv[2]
-  topModule   = "-top %s" % sys.argv[3]
-  if len(sys.argv) == 4:
-    define = ""
-  else:
-    define = "+define%s" % sys.argv[4]
+    # STEP: clean up
+    command = "mkdir -p build"
+    print("[command to run]: ", command)
+    assert os.system(command) == 0
 
+    command = f"rm -rf 'build/{build_folder_name}'"
+    print("[command to run]: ", command)
+    assert os.system(command) == 0
 
-  # STEP: clean up
-  command = "mkdir -p build"
-  print("[command to run]: ", command)
-  os.system(command)
-  command = "rm -r build/%s" % build_folder_name
-  print("[command to run]: ", command)
-  os.system(command)
+    entry_file = os.path.join(os.getcwd(), "scripts/verilator/top.sv")
+    # STEP: compile
+    with open(entry_file, "w") as top:
+        top.write(f'`include "{top_verilog}"')
 
+    command = (
+        f"verilator --cc --Mdir build/{build_folder_name}"
+        f" -I{includeDir} -top {topModule} +define{define}"
+        f" --prefix Vtop -Wno-fatal --trace --exe --build -j 4"
+        f" '{entry_file}'"
+        f" '{os.path.join(os.getcwd(), "scripts/verilator/main.cpp")}'"
+    )
+    print("[command to run]: ", command)
+    assert os.system(command) == 0
 
-  # STEP: compile
-  command = "echo \"\\`include \\\"%s\\\"\" > scripts/verilator/top.v" % top_verilog
-  print("[command to run]: ", command)
-  os.system(command)
+    command = f"rm '{entry_file}'"
+    print("[command to run]: ", command)
+    assert os.system(command) == 0
 
-  command = "verilator --cc --Mdir build/%s %s %s %s --prefix Vtop -Wno-fatal --trace --exe --build -j 4 %s %s" \
-            % (build_folder_name,
-               includeDir,
-               topModule,
-               define,
-               os.path.join(os.getcwd(), "scripts/verilator/top.v"),
-               os.path.join(os.getcwd(), "scripts/verilator/main.cpp"))
-  print("[command to run]: ", command)
-  os.system(command)
-  
-  command = "rm scripts/verilator/top.v"
-  print("[command to run]: ", command)
-  os.system(command)
-
-
-  # STEP: run
-  command = "build/%s/Vtop %s" % (build_folder_name, build_folder_name)
-  print("[command to run]: ", command)
-  os.system(command)
-
+    # STEP: run
+    command = f"build/{build_folder_name}/Vtop {build_folder_name}"
+    print("[command to run]: ", command)
+    assert os.system(command) == 0
